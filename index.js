cc213ff5150f2e6f6980e00df7de7630bd64ffb6
iammahir2020 <1810659@iub.edu.bd>
2022-05-25 16:14:40 +0600

    GET route for all products and DELETE route for deleting single product added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -143,9 +143,22 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/product", verifyJWTToken, async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/product", verifyJWTToken, async (req, res) => {
       const product = req.body;
       const result = await productsCollection.insertOne(product);
+      res.send(result);
+    });
+
+    app.delete("/product/:id", verifyJWTToken, async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
