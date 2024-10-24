cb0f877b24cc2d93bf32708bed1acdfe7614a205
iammahir2020 <mahiralkamal.mak@gmail.com>
2024-10-24 17:00:29 +0600

    fix

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const corsConfig = {
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 };
-app.use(cors(corsConfig));
+app.use(cors());
 
-app.options("*", cors(corsConfig));
+app.options("*", cors());
 app.use(express.json());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
